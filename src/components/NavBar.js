2fd1e315d99c244e397a016fa6c557ef41874b0f
Leon Kinyanjui Maingi <leon.kinyanjui@student.moringaschool.com>
2023-11-03 12:14:09 +0300

    Completed Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,32 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/" activeClassName="active">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/actors" activeClassName="active">
+            Actors
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/directors" activeClassName="active">
+            Directors
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/movies" activeClassName="active">
+            Movies
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
